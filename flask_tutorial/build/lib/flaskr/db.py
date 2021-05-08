348eb8c64dde95e9738784c3e97de9b231d166d4
nicework-jin <dev_seongjin@naver.com>
2021-05-08 21:51:03 +0900

    Update db.py

--- flask_tutorial/build/lib/flaskr/db.py
+++ flask_tutorial/build/lib/flaskr/db.py
@@ -1,10 +1,7 @@
 import sqlite3
-
-
 import click
 from flask import current_app, g
 from flask.cli import with_appcontext
-
 
 def get_db():
     if 'db' not in g:
@@ -16,20 +13,16 @@ def get_db():
 
     return g.db
 
-
 def close_db(e=None):
     db = g.pop('db', None)
     if db is not None:
         db.close()
-
-
 
 def init_db():
     db = get_db()
 
     with current_app.open_resource('schema.sql') as f:
         db.executescript(f.read().decode('utf8'))
-
 
 @click.command('init-db')
 @with_appcontext
